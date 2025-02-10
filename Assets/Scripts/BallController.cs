680c8267790ea7f5c7d7408f4118e4d519033aa6
Dylan <110944869+DyPickard@users.noreply.github.com>
2025-02-09 20:14:11 -0800

    fixed issue with launch indicator

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -27,6 +27,5 @@ public class BallManager : MonoBehaviour
         ballRB.isKinematic = false;
         ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
         launchIndicator.gameObject.SetActive(false);
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
