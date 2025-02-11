5a25e881eb69ce1e87a6127742d6d9cb24265996
Dylan <110944869+DyPickard@users.noreply.github.com>
2025-02-10 19:43:30 -0800

    Added Reset  Functionality and Fixed Gutter bug

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
-
-public class BallManager : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class BallController : MonoBehaviour
 {
     
     [SerializeField] private float force = 1f;
@@ -13,19 +13,31 @@ public class BallManager : MonoBehaviour
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
         inputManager.OnSpacePressed.AddListener(LaunchBall);
         transform.parent = ballAnchor;
         transform.localPosition = Vector3.zero;
         ballRB.isKinematic = true;
+        ResetBall();
     }
 
     private void LaunchBall()
     {
+        Debug.Log("Launch Ball method called.");
         if (isBallLaunched) return;
         isBallLaunched = true;
         transform.parent = null;
         ballRB.isKinematic = false;
         ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
         launchIndicator.gameObject.SetActive(false);
+    }
+    public void ResetBall()
+    {
+        Debug.Log("Reset Ball method called.");
+        isBallLaunched = false;
+        ballRB.isKinematic = true;
+        launchIndicator.gameObject.SetActive(true);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,54 @@
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputManager;
+    private GameObject pinObjects;
     private FallTrigger[] pins;
+
     private void Start()
     {
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+    private void HandleReset()
+    {
+        Debug.Log("Handle Reset method called.");
+        ball.ResetBall();
+        SetPins();
+    }
+    private void SetPins()
+    {
+        Debug.Log("Set Pins method called.");
+        // Destroy existing pins
+        if (pinObjects)
+        {
+            foreach (Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
+        }
+
+        // Instantiate new pins
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.Euler(0, -270, 0), transform);
         pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         foreach (FallTrigger pin in pins)
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
-    }
-    private void IncrementScore()
-    {
-        score++;
-        scoreText.text = $"Score: {score}";
     }
 }
